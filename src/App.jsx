510555c1a6eceaea21b0a73a95ffb3495232fe9f
Md. Nafiz Ahmed <nafizahmed000@gmail.com>
2025-12-10 10:20:11 +0600

    feat: add routes for all new dashboard pages

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,12 @@ import DashboardLayout from './layouts/DashboardLayout';
 import MyOrders from './pages/Dashboard/User/MyOrders';
 import MyProfile from './pages/Dashboard/User/MyProfile';
 import Invoices from './pages/Dashboard/User/Invoices';
+import MyWishlist from './pages/Dashboard/User/MyWishlist';
 import AddBook from './pages/Dashboard/Librarian/AddBook';
+import MyBooks from './pages/Dashboard/Librarian/MyBooks';
+import Orders from './pages/Dashboard/Librarian/Orders';
+import AllUsers from './pages/Dashboard/Admin/AllUsers';
+import ManageBooks from './pages/Dashboard/Admin/ManageBooks';
 import PrivateRoute from './routes/PrivateRoute';
 
 function App() {
@@ -32,10 +37,20 @@ function App() {
               
               {/* Dashboard Routes */}
               <Route path="/dashboard" element={<PrivateRoute><DashboardLayout /></PrivateRoute>}>
+                {/* User Routes */}
                 <Route path="my-orders" element={<MyOrders />} />
                 <Route path="profile" element={<MyProfile />} />
                 <Route path="invoices" element={<Invoices />} />
+                <Route path="wishlist" element={<MyWishlist />} />
+                
+                {/* Librarian Routes */}
                 <Route path="add-book" element={<AddBook />} />
+                <Route path="my-books" element={<MyBooks />} />
+                <Route path="orders" element={<Orders />} />
+                
+                {/* Admin Routes */}
+                <Route path="all-users" element={<AllUsers />} />
+                <Route path="manage-books" element={<ManageBooks />} />
               </Route>
             </Routes>
           </main>
